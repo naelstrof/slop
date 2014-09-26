a078a0df27231369e81741e64df41440bc979b63
Dalton Nell <naelstrof@gmail.com>
2014-09-25 19:11:03 -0600

    Whoops still learning git I suppose

--- options.cpp
+++ options.cpp
@@ -3,7 +3,7 @@
 slop::Options* options = new slop::Options();
 
 slop::Options::Options() {
-    m_version = "v2.0.1";
+    m_version = "v2.0.2";
     m_borderSize = 10;
     m_padding = 0;
     m_xdisplay = ":0";
