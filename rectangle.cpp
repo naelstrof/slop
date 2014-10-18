fa2b624c02c4061822f9a18e2a17e2f8b1737b13
Dalton Nell <naelstrof@gmail.com>
2014-10-18 12:43:07 -0600

    Added GPL notifications at the top of each file, and made it clear that contributors must agree with it too.

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,3 +1,22 @@
+/* rectangle.cpp: Handles creating rectangles on the screen.
+ *
+ * Copyright (C) 2014: Dalton Nell, Slop Contributors (https://github.com/naelstrof/slop/graphs/contributors).
+ *
+ * This file is part of Slop.
+ *
+ * Slop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Slop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Slop.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "rectangle.hpp"
 
 static Bool isDestroyNotify( Display* dpy, XEvent* ev, XPointer win ) {
--- x.hpp
+++ x.hpp
@@ -1,4 +1,22 @@
-// x.hpp: handles starting and managing X
+/* x.hpp: Handles starting and managing X.
+ *
+ * Copyright (C) 2014: Dalton Nell, Slop Contributors (https://github.com/naelstrof/slop/graphs/contributors).
+ *
+ * This file is part of Slop.
+ *
+ * Slop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Slop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Slop.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef IS_X_H_
 #define IS_X_H_
