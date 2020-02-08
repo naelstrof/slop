ddb368062159de6f2c6df406ef0bd4ccd2688a7f
xaos <xaos@xaos.tech>
2020-02-08 17:50:02 +0100

    wrote SlopStartDrag::update more verbose

--- src/slopstates.cpp
+++ src/slopstates.cpp
@@ -91,25 +91,35 @@ void slop::SlopStartDrag::onEnter( SlopMemory& memory ) {
 }
 
 void slop::SlopStartDrag::update( SlopMemory& memory, double dt ) {
-    char a = startPoint.y > mouse->getMousePos().y;
-    char b = startPoint.x > mouse->getMousePos().x;
-    char c = (a << 1) | b;
+    
+    //these are true when on the edges of the screen
     int xm = (mouse->getMousePos().x == 0 || mouse->getMousePos().x == WidthOfScreen(x11->screen)-1);
     int ym = (mouse->getMousePos().y == 0 || mouse->getMousePos().y == HeightOfScreen(x11->screen)-1);
-    switch ( c ) {
-        case 0: mouse->setCursor( XC_lr_angle );
-                memory.rectangle->setPoints(startPoint+glm::vec2(0,0), mouse->getMousePos()+glm::vec2(1*xm,1*ym));
-                break;
-        case 1: mouse->setCursor( XC_ll_angle );
-                memory.rectangle->setPoints(startPoint+glm::vec2(0,0), mouse->getMousePos()+glm::vec2(1*xm,1*ym));
-                break;
-        case 2: mouse->setCursor( XC_ur_angle );
-                memory.rectangle->setPoints(startPoint+glm::vec2(0,1*ym), mouse->getMousePos()+glm::vec2(1*xm,0));
-                break;
-        case 3: mouse->setCursor( XC_ul_angle );
-                memory.rectangle->setPoints(startPoint+glm::vec2(1*xm,1*ym), mouse->getMousePos()+glm::vec2(0,0));
-                break;
+    
+    // set cursor and compensate edges somehow
+    bool curserAbove = startPoint.y > mouse->getMousePos().y;
+    bool cursorLeft = startPoint.x > mouse->getMousePos().x;
+
+    if ( curserAbove ) {
+        if ( cursorLeft ) {
+            mouse->setCursor( XC_ul_angle );
+            memory.rectangle->setPoints(startPoint+glm::vec2(1*xm,1*ym), mouse->getMousePos()+glm::vec2(0,0));        }
+        else {
+            mouse->setCursor( XC_ur_angle );
+            memory.rectangle->setPoints(startPoint+glm::vec2(0,1*ym), mouse->getMousePos()+glm::vec2(1*xm,0));
+        }
     }
+    else {
+        if ( cursorLeft ) {
+            mouse->setCursor( XC_ll_angle );
+            memory.rectangle->setPoints(startPoint+glm::vec2(0,0), mouse->getMousePos()+glm::vec2(1*xm,1*ym));
+        }
+        else {
+            mouse->setCursor( XC_lr_angle );
+            memory.rectangle->setPoints(startPoint+glm::vec2(0,0), mouse->getMousePos()+glm::vec2(1*xm,1*ym));
+       }
+    }
+
     if ( !mouse->getButton( 1 ) ) {
         memory.setState( (SlopState*)new SlopEndDrag() );
     }
